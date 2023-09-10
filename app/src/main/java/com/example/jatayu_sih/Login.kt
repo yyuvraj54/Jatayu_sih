89e1b7b94c0d581ec1e4406478035dea53b76152
Yuvraj <yyuvraj54@gmail.com>
2023-09-10 13:39:08 +0530

    Bottom nav added with 4 Fragments

--- app/src/main/java/com/example/jatayu_sih/Login.kt
+++ app/src/main/java/com/example/jatayu_sih/Login.kt
@@ -1,5 +1,6 @@
 package com.example.jatayu_sih
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,9 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/jatayu_sih/MainActivity.kt
+++ app/src/main/java/com/example/jatayu_sih/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.jatayu_sih
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val bottomNav= findViewById<BottomNavigationView>(R.id.bottom_navbar)
+
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.operation_fragment,
+                R.id.notification_fragment,
+                R.id.logs_fragment,
+                R.id.profile_fragment
+            )
+        )
+
+        bottomNav.setupWithNavController(navController)
+
+
+
     }
 }
