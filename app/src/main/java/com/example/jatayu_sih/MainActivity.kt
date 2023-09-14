e9af0e86b767b2e0dc9d381e49e4538512f781ce
Yuvraj <yyuvraj54@gmail.com>
2023-09-14 18:12:11 +0530

    Location Background Service Added

--- app/src/main/java/com/example/jatayu_sih/MainActivity.kt
+++ app/src/main/java/com/example/jatayu_sih/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.jatayu_sih
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -14,6 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            0
+        )
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController: NavController = navHostFragment.navController
