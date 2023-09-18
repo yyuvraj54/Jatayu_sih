6497397d88af7330adbc1544aa004a55a8b20479
Vibhuti Kapoor <vibhutikapoor22@gmail.com>
2023-09-19 04:10:08 +0530

    Profile fragment

--- app/src/main/java/com/example/jatayu_sih/nav_fragment/profile_fragment.kt
+++ app/src/main/java/com/example/jatayu_sih/nav_fragment/profile_fragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.example.jatayu_sih.Login
 import com.example.jatayu_sih.MainActivity
 import com.example.jatayu_sih.R
@@ -16,6 +17,7 @@ import com.example.jatayu_sih.loginStatus
 
 class profile_fragment : Fragment() {
 
+    private lateinit var prefs: loginStatus
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +25,25 @@ class profile_fragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_profile_fragment, container, false)
 
-
+        prefs = loginStatus(requireContext())
+        //val isLoggedIn = prefs.isLoggedIn
+        val userId = prefs.userId
+        val username = prefs.username.toString()
+        val password = prefs.password.toString()
+        val token = prefs.token.toString()
+        val userRole = prefs.userRole.toString()
+        val team = prefs.team.toString()
+        val organisation = prefs.organisation.toString()
         val logout=view.findViewById<Button>(R.id.btnLogOut)
+
+
+        val textViewUserId = view.findViewById<TextView>(R.id.username)
+        val textViewUsername = view.findViewById<TextView>(R.id.userId)
+
+        // Set text for TextViews
+        textViewUsername.text = "$username"
+        textViewUserId.text = "$userId"
+
 
         logout.setOnClickListener {
             val prefs = loginStatus(requireContext())
@@ -34,12 +53,6 @@ class profile_fragment : Fragment() {
             activity?.finish()
 
         }
-
-
-
-
-
-
 
         return view
     }
